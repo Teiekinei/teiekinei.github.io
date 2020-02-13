aac0c87dd5d5ebe7a58d3aca3ad071b94d6bd2da
Teiekinei <teiekinei.rofye@gmail.com>
2020-02-13 19:03:04 +0800

    SaLi Online

---
+++ It's_SaLi/js/load.js
@@ -0,0 +1,8 @@
+﻿(function(){
+	var online = /asus\.com/.test( window.location.hostname ), path = online?'/':'';
+	document.write( '<link rel="stylesheet" href="' + path + '/css/main.css">');
+	document.write( '<script src="' + path + '/js/main.js"></script>');
+})();
+
+
+
---
+++ It's_SaLi/js/main.js
@@ -0,0 +1,61 @@
+﻿
+(function(){
+	var pageInit=function(){
+		$window=$(window);
+		$wp=$(".overview-wrapper");
+		$rwf=$("#rog-width-full");
+		$sso=$("#special-sectionOverview");
+
+		$window.load(pageResize); //works in chrome
+		$window.resize(pageResize);
+	}
+	var pageResize=function(){
+		$sso.css({"height":$rwf.height()});
+	};
+	$(document).ready(function($) {
+		pageInit();	
+	});
+
+})();
+
+
+
+
+$(function(){
+    $('.back-to-top').click(function(){
+        var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
+        $body.animate({
+                scrollTop: 0
+        }, 800);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+//Scroll indicate
+$(window).load(function(){
+    $(window).scroll(function(){
+        var a = $(window).scrollTop();
+                            
+        if(a > 1000){
+            $(".back-to-top").fadeIn();
+        }else{
+            $(".back-to-top").fadeOut();
+        }
+    });
+});
+
+
+
+
+
+
+
+
